2c33996217b3879dbe7da40cec785e0d4d3003d8
Damsul <gyoung9707@naver.com>
2023-04-19 11:49:34 +0900

    feat: first commit - java Project 빌드

---
+++ src/main/java/com/example/javaproject2/PrintHelloTest.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2;
+
+public class PrintHelloTest {
+
+    public static void main(String[] args) {
+        PrintHello printHello = new PrintHello();
+        printHello.print();
+    }
+
+}

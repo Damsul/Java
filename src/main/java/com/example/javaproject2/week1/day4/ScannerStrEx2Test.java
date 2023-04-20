48080ad7747a492c0c6d83199e61740657eb839e
Damsul <gyoung9707@naver.com>
2023-04-20 10:44:04 +0900

    feat: ScannerStrEx2 클래스 생성

---
+++ src/main/java/com/example/javaproject2/week1/day4/ScannerStrEx2Test.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1.day4;
+
+public class ScannerStrEx2Test {
+
+    public static void main(String[] args) {
+        ScannerStrEx2 scannerStrEx2 = new ScannerStrEx2();
+        scannerStrEx2.inputStrAndPlus();
+    }
+
+}

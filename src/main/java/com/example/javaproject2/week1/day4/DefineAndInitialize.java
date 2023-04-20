4e16e8b56f97655510b7abb18660f879a1dfd6b3
Damsul <gyoung9707@naver.com>
2023-04-20 15:37:29 +0900

    feat: DefineAndInitialize 클래스 생성 - 변성 선언 및 초기화 한번에 진행

---
+++ src/main/java/com/example/javaproject2/week1/day4/DefineAndInitialize.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week1.day4;
+
+import com.example.javaproject2.PrintHello;
+
+public class DefineAndInitialize {
+
+    public static void main(String[] args) {
+        int iVal = 1; // 변수 선언 및 초기화
+        PrintHello printHello = new PrintHello();
+
+
+    }
+
+}

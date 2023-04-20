b0e7385d1ce5fc1d6f3673fb2f94379cf4c4d06a
Damsul <gyoung9707@naver.com>
2023-04-20 11:51:05 +0900

    feat: PrintDate 클래스 생성 - 자릿수 포맷 형식

---
+++ src/main/java/com/example/javaproject2/week1/day4/PrintfDate.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week1.day4;
+
+public class PrintfDate {
+
+    public static void main(String[] args) {
+        System.out.printf("%d\n", 9);
+        System.out.printf("%d\n", 10);
+        System.out.printf("%02d\n", 5);
+        System.out.printf("%d-%02d-%02d", 2023, 9, 15);
+    }
+
+}

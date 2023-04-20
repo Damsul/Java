264960499182740fd9c7765b3ac63786bca060c1
Damsul <gyoung9707@naver.com>
2023-04-20 14:47:19 +0900

    fix: 변수명 변경

--- src/main/java/com/example/javaproject2/week1/day4/ScannerFormatting.java
+++ src/main/java/com/example/javaproject2/week1/day4/ScannerFormatting.java
@@ -6,9 +6,9 @@ public class ScannerFormatting {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.printf("%d와 %d의 합은 %d입니다.", a, b, a + b);
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+        System.out.printf("%d와 %d의 합은 %d입니다.", val1, val2, val1 + val2);
     }
 
 }

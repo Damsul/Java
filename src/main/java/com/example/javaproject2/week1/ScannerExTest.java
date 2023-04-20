20a4d55ea44d76dc3b8370d41e81ae66d1ddfd11
Damsul <gyoung9707@naver.com>
2023-04-20 09:23:29 +0900

    feat:ScannerEx2 클래스 생성 - Scanner로 값 2개 입력 받아서 더한 결과 출력 하기

--- src/main/java/com/example/javaproject2/week1/ScannerExTest.java
+++ src/main/java/com/example/javaproject2/week1/ScannerExTest.java
@@ -3,8 +3,11 @@ package com.example.javaproject2.week1;
 public class ScannerExTest {
 
     public static void main(String[] args) {
-        ScannerEx se = new ScannerEx();
-        se.readTwoNumbersAndPlus();
+//        ScannerEx se = new ScannerEx();
+//        se.readTwoNumbersAndPlus();
+
+        ScannerEx2 se2 = new ScannerEx2();
+        se2.readTwoNumbersAndPlus();
     }
 
 }

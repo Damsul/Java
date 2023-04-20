b5a406b478845c17e743af8dd00a361d2531dda1
Damsul <gyoung9707@naver.com>
2023-04-20 09:40:56 +0900

    feat:ScannerStrEx 클래스 생성 - Scanner로 문자열 2개 입력 받아서 더한 결과 출력 하기

---
+++ src/main/java/com/example/javaproject2/week1/ScannerStrEx.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week1;
+
+import java.util.Scanner;
+
+public class ScannerStrEx {
+
+    void inputStrAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next() + sc.next());
+    }
+}

8abffd62bd19d401831064707fc663d45a7b3d8b
Damsul <gyoung9707@naver.com>
2023-04-21 10:43:35 +0900

    feat: CodeUp 1017번 문제 풀이

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1017.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1017 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+        System.out.printf("%d %d %d", val1, val1, val1);
+    }
+
+}

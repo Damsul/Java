5e0a91896a55186d87ea9f3c45ba8d1314967294
Damsul <gyoung9707@naver.com>
2023-04-25 10:13:28 +0900

    feat: CodeUp 1031~1037 문제 풀이

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1033.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1033 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int data = sc.nextInt();
+        System.out.printf("%X", data);
+    }
+
+}

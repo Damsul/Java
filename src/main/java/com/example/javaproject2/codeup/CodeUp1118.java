a6be9abd24a1336031bcbd09eb7afb0af4329192
Damsul <gyoung9707@naver.com>
2023-04-28 22:07:02 +0900

    feat: CodeUp 문제 풀이 - 1071~1082 - 1116~1124 - 1441

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1118.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1118 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+//        float val2 = sc.nextFloat();
+        System.out.printf("%.1f", val1 * val2 / 2.0);
+    }
+}

0b020b15e38ee864b323c59294cc564c60524c60
Damsul <gyoung9707@naver.com>
2023-05-01 17:12:42 +0900

    feat: for 반복문, for 이중 반목문 - 아스키코드 - 구구단 2단 출력 - 구구단 2~4단 동시 출력 - 메서드를 이용한 구구단 출력 - 변수에 누적 합

---
+++ src/main/java/com/example/javaproject2/week3/MultiplicationTableStep2.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week3;
+
+public class MultiplicationTableStep2 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("2 * %d = %d\n", i, i * 2);
+        }
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week3/MultiplicationTableStep6.java
@@ -0,0 +1,17 @@
+package com.example.javaproject2.week3;
+
+public class MultiplicationTableStep6 {
+
+    public static void danPrint(int dan) {
+        for (int j = 1; j <= 9; j++) {
+            System.out.printf("%d * %d = %d\n", dan, j, dan * j);
+        }
+        System.out.println("----------");
+    }
+    public static void main(String[] args) {
+        danPrint(2);
+        danPrint(4);
+        danPrint(8);
+        danPrint(9);
+    }
+}

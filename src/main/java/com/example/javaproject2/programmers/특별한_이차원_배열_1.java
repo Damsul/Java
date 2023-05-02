6ca31c1e23e4e20ad75aefc25334ecf127ccdb64
Damsul <gyoung9707@naver.com>
2023-05-02 21:26:23 +0900

    feat: 프로그래머스 문제 풀이 - 배열 만들기 2 - 수열과 구간 뭐리 2~4 - 자릿수 더하기 - 특별한 이차원 배열 1

---
+++ src/main/java/com/example/javaproject2/programmers/특별한_이차원_배열_1.java
@@ -0,0 +1,22 @@
+package com.example.javaproject2.programmers;
+
+public class 특별한_이차원_배열_1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer[0].length; j++) {
+                if (i == j) {
+                    answer[i][j] = 1;
+                }
+            }
+        }
+
+        return answer;
+    }
+}

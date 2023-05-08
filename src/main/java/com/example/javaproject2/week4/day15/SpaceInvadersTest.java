7a84e1c96f3660572b0bfb65b35ef3ee841e5609
Damsul <gyoung9707@naver.com>
2023-05-08 11:29:25 +0900

    feat: 메서드 실습 1 - SpaceInvaders 클래스 생성 - moveLeft, moveRight 메서드 사용

---
+++ src/main/java/com/example/javaproject2/week4/day15/SpaceInvadersTest.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.week4.day15;
+
+public class SpaceInvadersTest {
+
+    public static void main(String[] args) {
+        SpaceInvaders si = new SpaceInvaders();
+        si.moveLeft();
+        System.out.println(si.location);
+        si.moveLeft();
+        System.out.println(si.location);
+        si.moveLeft();
+        System.out.println(si.location);
+        si.moveRight();
+        System.out.println(si.location);
+    }
+}

05c6e0130555ca1ce5056e84603c946f1591c06e
Damsul <gyoung9707@naver.com>
2023-05-08 20:08:08 +0900

    feat: 메서드 실습 3 - 메서드 구성 요소 - 접근제어자: Greet, GreetTest - 리턴타입: VariousReturnType - 메소드명

---
+++ src/main/java/com/example/javaproject2/week4/day15/GreetTest.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week4.day15;
+
+public class GreetTest {
+    public static void main(String[] args) {
+        Greet greet = new Greet();
+        greet.printHello();
+        System.out.println(greet);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week4/day15/VariousReturnType.java
@@ -0,0 +1,28 @@
+package com.example.javaproject2.week4.day15;
+
+public class VariousReturnType {
+
+    public boolean isAdult() {
+        return true;
+    }
+
+    public int plus() {
+        return 1 + 1;
+    }
+
+    public User getUser() {
+        return new User();
+    }
+
+    public void printHello() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        VariousReturnType vrt = new VariousReturnType();
+        boolean adult = vrt.isAdult();
+        int plus = vrt.plus();
+        User user = vrt.getUser();
+        vrt.printHello();
+    }
+}

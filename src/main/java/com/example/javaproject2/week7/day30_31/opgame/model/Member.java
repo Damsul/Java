c810fca6fcf4239c8ca6c57a6f8ce50d3b352e8b
Damsul <gyoung9707@naver.com>
2023-06-01 20:01:42 +0900

    feat: Member DAO, DTO 구현

--- src/main/java/com/example/javaproject2/week7/day30_31/opgame/model/Member.java
+++ src/main/java/com/example/javaproject2/week7/day30_31/opgame/model/Member.java
@@ -1,11 +1,19 @@
 package com.example.javaproject2.week7.day30_31.opgame.model;
 
 public class Member {
+    String id;
+    boolean isLogined;
+
+    public Member(String id, boolean isLogined) {
+        this.id = id;
+        this.isLogined = isLogined;
+    }
+
     public String getId() {
-        return null;
+        return id;
     }
 
     public boolean isLogined() {
-        return false;
+        return isLogined;
     }
 }

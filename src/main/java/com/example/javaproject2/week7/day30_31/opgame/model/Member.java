c8cdc7c41e8411e57e30aaf9ace2d0ad2dee9e0f
Damsul <gyoung9707@naver.com>
2023-06-01 19:28:17 +0900

    feat: Member DAO, DTO 시작(테스트x)

---
+++ src/main/java/com/example/javaproject2/week7/day30_31/opgame/model/Member.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week7.day30_31.opgame.model;
+
+public class Member {
+    public String getId() {
+        return null;
+    }
+
+    public boolean isLogined() {
+        return false;
+    }
+}
